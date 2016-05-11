4a6dd07aa777849fcfc3b22d97013130824d0824
Bernd Eickhoff <beickhoff@avallain.com>
2016-05-11 15:39:37 +0200

    pristine rails 4.2.6

---
+++ test/factories/users.rb
@@ -0,0 +1,16 @@
+FactoryGirl.define do
+  factory :user do
+    first_name "MyString"
+last_name "MyString"
+email "MyString"
+password_digest "MyString"
+seed "MyString"
+session_token "MyString"
+signon_token "MyString"
+reset_password_token "MyString"
+sign_in_at "2016-05-11 15:28:18"
+sign_in_ip "MyString"
+sign_in_count 1
+  end
+
+end
