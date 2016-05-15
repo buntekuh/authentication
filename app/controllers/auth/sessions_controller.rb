cdf1aa36dbc23556d3e9ecc88b4affdec395ca0a
Bernd Eickhoff <beickhoff@avallain.com>
2016-05-15 13:49:09 +0200

    sessions_controller create (sign in) method

--- app/controllers/auth/sessions_controller.rb
+++ app/controllers/auth/sessions_controller.rb
@@ -9,6 +9,20 @@ module Auth
     # Displays the sign in form
     def new
       @form = UserPasswordAuthenticationForm.new
-    end    
+    end
+
+    #
+    # Signs the user in or redirects back to the sign in form
+    #
+    def create
+      begin
+        @form = UserPasswordAuthenticationForm.new(params[:session])
+        user = UserPasswordAuthenticationCommand.execute(@form)
+        set_current_user(user)
+        redirect_to welcome_path
+      rescue UserPasswordAuthenticationCommand::InvalidCredentials, UserPasswordAuthenticationCommand::InvalidForm => e
+        redirect_to root_path(email: @form.email, sign_in: 'failed')
+      end
+    end
   end
 end
--- test/controllers/auth/sessions_controller_test.rb
+++ test/controllers/auth/sessions_controller_test.rb
@@ -2,9 +2,38 @@ require 'test_helper'
 
 module Auth
   class SessionsControllerTest < ActionController::TestCase
+    setup do
+      @email        = 'hsulu@starfleet.ufp'
+      @password     = 'soSpooky'
+      Users::CreateCommand.execute(Users::CreateForm.new(
+       first_name: 'Hikaru', 
+        last_name:  'Sulu', 
+        email:      @email, 
+        password:   @password
+      ))
+      @controller.send :sign_out
+
+    end
+
     test 'new' do
       get :new
       assert_includes response.body, '<input type="text" name="session[email]"'
+    end
+
+    test 'create signs in and redirects to inside controller' do
+      post :create, session: { email: @email, password: @password }
+      assert @controller.send('signed_in?')
+      assert_equal @controller.send('current_user'), User.find_by_email(@email)
+      assert_redirected_to welcome_path
+    end
+
+    test 'create redirects to new if credentials empty or wrong' do
+      [{email: @email, password: ''}, {email: '', password: @password}, {email: nil, password: nil}].each do |params|
+        post :create, session: params
+        assert_not @controller.send('signed_in?')
+        assert_not @controller.send('current_user')
+        assert_redirected_to root_path(sign_in: 'failed', email: params[:email])
+      end
     end
   end
 end
